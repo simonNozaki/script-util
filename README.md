2aa22d3b5df483accb099b1bcbe2839c23dd437c
snozaki-at-898942717313 <s.nozaki@mac0087.local>
2019-05-30 08:48:37 +0900

    gulpの導入

---
+++ README.md
@@ -0,0 +1,7 @@
+# utiljs
+よく使う定数、ユーティリティのパッケージ。  
+配列操作、文字列操作、オブジェクト操作を提供する。
+
+# usage
+呼び出し。  
+`const utiljs = require("utiljs");`  
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const gulp = require("gulp");
+const typescript = require("gulp-typescript");
+
+gulp.task("default", async function(){
+    console.log("gulp default task");
+});
+
+gulp.task("test", async function(){
+
+});
+
+/**
+ * コンパイル
+ */
+gulp.task("assemble",  async function(){
+    gulp.src("src/*.ts")
+        .pipe(typescript())
+        .pipe(gulp.dest("dist"))
+});
